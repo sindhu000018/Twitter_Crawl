db2ba281e3a9aac21adf5c121c0bf93ae5fe38c2
SindhuReddy KalathurGopal <51187172+sindhu000018@users.noreply.github.com>
2020-05-14 06:51:58 +0530

    Update Twitter_scrape.py

--- Twitter_scrape.py
+++ Twitter_scrape.py
@@ -1,16 +1,18 @@
 !pip install twitter_scraper
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 tweet_data = []
+duration = datetime.today() - timedelta(days=7)
 groupLists=['FactlyIndia','TimesFactCheck','QuintFactCheck']
 dateTime = datetime.now()
 fileName = 'data_twitter_'+str(dateTime)+'.csv'
 for item in groupLists:
-  for tweet in get_tweets(item):
-    temp_dict=dict(tweet)
-    tweet_data.append(temp_dict)
-  # print(tweet)
+  for tweet in get_tweets(item, pages=20):
+    post_date = (tweet['time'])
+    if post_date > duration: 
+      temp_dict=dict(tweet)
+      tweet_data.append(temp_dict)
 tweet_df = pd.DataFrame(tweet_data)
 tweet_df['scrapedDate'] = dateTime
 tweet_df.head()
